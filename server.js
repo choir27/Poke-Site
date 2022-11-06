a8debdccc77ab3cdfd277d59175860a43dc498a3
Richard <66279068+choir27@users.noreply.github.com>
2022-11-06 12:28:37 -0500

    Update server.js

--- server.js
+++ server.js
@@ -71,5 +71,5 @@ response.json(pokemonAnime)
 
 
 app.listen(process.env.PORT, ()=>{  
-    console.log(`Server running on port ${PORT}`)
+    console.log(`Server running on port ${process.env.PORT}`)
 })
